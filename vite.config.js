2c0141919673f8c496c03cd2d2f8edde94aba50c
Giovany Victor Mota <giovany.victor.mota34@gmail.com>
2025-09-25 22:30:12 +0000

    final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Adicione esta configuração para permitir o acesso do CodeSandbox
+    allowedHosts: [".csb.app"],
+  },
+});
